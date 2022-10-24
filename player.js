752f05297a43b33ee7ce51d845386578c60a7fb1
Andrés Crespo <acrerosj@canariaseducacion.es>
2022-10-24 19:15:01 +0100

    Player sex y birthday

--- player.js
+++ player.js
@@ -1,9 +1,22 @@
 class Player {
   name;
 
-  constructor(name) {
+  #sex = 'female';
+
+  birthday = null;
+
+  constructor(name, sex, strDate) {
     this.name = name;
+    if (sex === 'male' || sex === 'female') {
+      this.#sex = sex;
+    }
+    if (strDate) {
+      const [d, m, y] = strDate.split('/');
+      this.birthday = new Date(y, m, d);
+    }
   }
+
+  get sex() { return this.#sex; }
 }
 
 module.exports = Player;
--- player.test.js
+++ player.test.js
@@ -11,3 +11,30 @@ test('Crear jugador con nombre, se almacena nombre en propiedad name', () => {
   const player1 = new Player('Pepe');
   expect(player1.name).toBe('Pepe');
 });
+
+test('Al crear un jugador se debe indicar si es "male" o "female".', () => {
+  const player1 = new Player('Pepe', 'male');
+  expect(player1.sex).toBe('male');
+  const player2 = new Player('Ana', 'female');
+  expect(player2.sex).toBe('female');
+});
+
+
+test('Si no se indica nada o no es exactamente "male" o "female", se almacena como "female".', () => {
+  const player1 = new Player('Raro','kkfu');
+  expect(player1.sex).toBe('female');
+  const player2 = new Player('Ana');
+  expect(player2.sex).toBe('female');
+});
+
+test('No se puede modificar el sexo.', () => {
+  const player2 = new Player('Ana', 'female');
+  player2.sex = 'male';
+  expect(player2.sex).toBe('female');
+});
+
+test('Establecer fecha nacimiento jugador', () => {
+  const player1 = new Player('Pepe', 'male', '23/12/2021');
+  const date1 = new Date(2021, 12, 23);
+  expect(player1.birthday).toEqual(date1);
+});
